e2b179d588ac778adcac1956c431c4bbcedc3c6b
mosefatullah <mosefatullah@gmail.com>
2024-08-07 13:01:14 +0600

    first commit

---
+++ try.py
@@ -0,0 +1,7 @@
+print("DodleUI v1.0.0!")
+s = input("Would you like to install DodleUI? (y/n): ")
+if s == "y":
+  print("Installing...")
+  for i in range(0, 100):
+    print("Installing... (" + str(i) + "%)")
+  print("DodleUI has been installed!")
